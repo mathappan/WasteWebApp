caffb33fa6450d4aa3fd22836447a0e7432a7183
Mathew Paul <mathewpk13@gmail.com>
2017-07-29 07:19:41 +0100

    Added a Minimum Value Filer

--- mysite/maps/static/maps/maps.jsx
+++ mysite/maps/static/maps/maps.jsx
@@ -3,6 +3,7 @@ var geojson;
 var material;
 var geoJsonTopology;
 var mouselayer;
+var minimumValue;
 
 mymap = L.map('mapid').setView([54.36,-2.4],6 );
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', 
@@ -51,10 +52,10 @@ function onEachFeature(feature, layer) {
 
 
 function getColor(d) {
-        return  d > 50 ? '#006837' :
-                d >  10 ? '#31a354' :
-                d >  5 ? '#78c679' :
-                d > 0 ? '#c2e699' :
+        return  (d > 50 && d > minimumValue) ? '#006837' :
+                (d >  10 && d > minimumValue) ? '#31a354' :
+                (d > 5 && d > minimumValue) ? '#78c679' :
+                (d > 0 && d > minimumValue)? '#c2e699' :
                 '#ffffcc';
 }
 
@@ -88,13 +89,38 @@ class SelectionForm extends React.Component {
 
         render() {
             return (
-                <form onChange={this.handleChangeMaterialOption} id='MaterialForm'>
-                    <select>
-                        <option value='Glass' >Glass</option>
-                        <option value='Plastic' >Plastic</option>
-                    </select>
-                </form>
+                    <form onChange={this.handleChangeMaterialOption} id='MaterialForm'>
+                        <select>
+                            <option value='Glass' >Glass</option>
+                            <option value='Plastic' >Plastic</option>
+                        </select>
+                    </form>
             )                                                   	
+        }
+}
+
+
+class MinimumValueFilter extends React.Component {
+
+        constructor(props) {
+            super(props);
+            this.handleChangeMinimumValue = this.handleChangeMinimumValue.bind(this);
+        }
+
+        handleChangeMinimumValue(e) {
+            let changedMinimumValue =e.target.value;
+            this.props.changeMinimumValue(changedMinimumValue);
+        }
+    
+        render () {
+                return (
+                        <form onChange={this.handleChangeMinimumValue} id='MinimumValueForm'>
+                                <input type='range' />
+                        </form>
+
+
+
+            )
         }
 }
 
@@ -103,31 +129,45 @@ class Map extends React.Component {
         constructor(props) {
                 super(props);
                 this.changeMaterialOption = this.changeMaterialOption.bind(this);
-                this.changeStyle = this.changeStyle.bind(this);     
-                this.state = { material: 'Select Material',
+                this.changeStyle = this.changeStyle.bind(this);
+                this.changeMinimumValue = this.changeMinimumValue.bind(this)     
+                this.state = { 
+                        material: 'Select Material',
+                        minimumValue: 0,
                 };
                 
         }
         
         changeMaterialOption(changedMaterial) {
             
-            this.setState ({
-                material: changedMaterial,
-            });
-             console.log('asd');
-            material = changedMaterial;
-            geojson.setStyle(style); 
+                this.setState ({
+                    material: changedMaterial,
+                });
+                console.log('asd');
+                material = changedMaterial;
+                geojson.setStyle(style); 
         }
 
         changeStyle() {
                
         }
 
+        changeMinimumValue(newMinimumValue) {
+                this.setState ({
+                    minimumValue: newMinimumValue,
+                })
+                minimumValue = newMinimumValue;
+                geojson.setStyle(style);
+
+        }
+
         render() {
             return (
                         <div>
                             <SelectionForm changeMaterialOption = {this.changeMaterialOption} changeStyle = {this.changeStyle} />
-                            <p>{this.state.material}</p>
+                            <MinimumValueFilter changeMinimumValue = {this.changeMinimumValue} />
+                            <p> {this.state.material} </p>
+                            <p> {this.state.minimumValue} </p>
                         </div>
             )
         }
